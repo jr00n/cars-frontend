b6e7f14faf05a063b62fa212864de7e7756ef966
Jeroen Wolff <jeroen.wolff@gmail.com>
2019-10-16 11:47:09 +0200

    Basic CRUD applicaton frontend demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { version } from '../../package.json';
+import { name } from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  // title = 'CarList Demo FrontEnd';
+  public title: string  = name;
+  public version: string = version;
 }
